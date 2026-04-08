2e0986007eacc4883a4f6263e61766d7f33de877
prettybuffy <160278469+prettybuffy@users.noreply.github.com>
2026-04-08 11:58:21 -0700

    upload vibe-coded about-me game
    1) all html files in public folder 2) added photos in public/photo folder 3) comment out path so its only accessible thru link

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -24,6 +24,7 @@ export default function Header() {
             <Link to="/" className="pr-4 py-3 hover:text-highlight transition-colors">Home</Link>
             <Link to="/papers" className="px-4 py-3 hover:text-highlight transition-colors">Papers</Link>
             <Link to="/projects" className="px-4 py-3 hover:text-highlight transition-colors">Projects</Link>
+             {/*<a href="/about-me/" className="px-4 py-3 hover:text-highlight transition-colors">About Me</a>*/} 
             {/*<Link to="/teaching" className="px-4 py-3 hover:text-highlight transition-colors">Teaching</Link>*/}
           </div>
         </div>
